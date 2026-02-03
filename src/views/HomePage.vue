3d1bf5f25f4664348a5d77a20f38b4485af26c0c
Olorunfemi Samuel <forfemi@gmail.com>
2026-02-03 12:00:54 +0100

    Fixed Font issue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,7 @@
     <div class="max-w-7xl mx-auto w-full px-4 sm:px-6 md:px-20 lg:px-32 xl:px-40 2xl:px-48 py-12 md:py-0 grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-8 lg:gap-12 xl:gap-16 items-center">
       <!-- LEFT: Text Content -->
       <div class="text-left space-y-4 sm:space-y-6 md:space-y-8">
-        <h1 class=" font-lince font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-[#645162] leading-tight">
+        <h1 class=" font-lince font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl text-[#645162] leading-tight">
           Technology Cornerstone <br />
           <span class="text-[#645162]">In Africa</span>
         </h1>
